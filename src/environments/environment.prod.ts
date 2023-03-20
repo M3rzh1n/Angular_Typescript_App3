0c18fd041ceaf51299c9c3c376009eb7dc9f1071
M3rzh1n <96054590+M3rzh1n@users.noreply.github.com>
2023-03-20 11:36:57 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
   production: true,
+  auth0: {
+    domain: 'dev-isoft.us.auth0.com',
+    clientId: 'lERzBtf7cxB37irvf5ON8ZPjyXXnYYyR',
+    authorizationParams: {
+      redirect_uri: 'https://m3rzh1n.github.io/Angular_Typescript_App3/callback',
+      connection: "email",
+    },
+    errorPath: '/callback',
+  },
+  api: {
+    serverUrl: '',
+  },
 };
