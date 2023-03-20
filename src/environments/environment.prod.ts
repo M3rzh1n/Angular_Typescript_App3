2d339a60ce73a78766f7ed9068dc0b6f0e5ed623
M3rzh1n <96054590+M3rzh1n@users.noreply.github.com>
2023-03-20 12:09:22 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   auth0: {
     domain: 'dev-isoft.us.auth0.com',
-    clientId: 'lERzBtf7cxB37irvf5ON8ZPjyXXnYYyR',
+    clientId: 'MsRerHg8873JiTXZzpwxkNKrjBoEi3p3',
     authorizationParams: {
       redirect_uri: 'https://m3rzh1n.github.io/Angular_Typescript_App3/callback',
       connection: "email",
